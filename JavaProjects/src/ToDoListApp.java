fcbb3e64a405b541e26eb0dc9c0eb62f34016a54
Eljen Mae Augusto <86765650+EljenMae@users.noreply.github.com>
2025-08-13 21:55:59 +0800

    adding the guess number game

---
+++ JavaProjects/src/ToDoListApp.java
@@ -0,0 +1,47 @@
+import java.util.Scanner; // input of the numbers
+import java.util.ArrayList; //creating an array list
+
+public class ToDoListApp {
+        public static void main(String[] args) {
+
+            Scanner input = new Scanner(System.in);
+            ArrayList<String> tasks = new ArrayList<>(); //storing the list
+            boolean running = true; //this is for the boolean if become true
+            boolean returnFromTheStart = true;
+
+                System.out.println("=== TO-DO LIST APP ===");
+                System.out.println("1. View Task ");
+                System.out.println("2. Add Task ");
+                System.out.println("3. Remove Task ");
+                System.out.println("4. Exit ");
+                System.out.print("Choose an option: ");
+                int choices = input.nextInt();
+
+                //conditional for the choices
+            if (choices == 1){
+                System.out.println("Your task:");
+
+                do{
+                    System.out.print("Do you want to continue? (Yes or no)");
+                    returnFromTheStart = input.nextLine();
+
+                    //if (returnFromTheStart.equals("Yes"))
+                }
+
+            }else if (choices == 2){
+                System.out.println("Enter new task: ");
+                String task = input.nextLine();
+                tasks.add(task);
+                System.out.println("Task added successfully!");
+
+
+            }else if (choices == 3){
+                System.out.println("Remove Task");
+
+            }else if (choices == 4){
+                System.out.println("Exit");
+            }
+
+        }
+}
+
